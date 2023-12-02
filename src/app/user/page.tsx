64a105e213903dc84f56ffe60971dac709ea8879
dorm <palapon2546@gmail.com>
2023-12-02 21:24:10 +0700

    initial commit

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -7,20 +7,18 @@ import React from 'react'
 //   )
 // }
 
-const hhome = () => {
+const register = () => {
     return(
-      <div className='boxx'>
+        <div className='boxx'>
         <div>Username :</div>
         <input type='string'></input>
         <div>Password :</div>
         <input type='password'></input>
         <div className='login'>
-          <button>Login</button>
-        </div>
-        <div className='register'>
-          <Link href="/user">Register ?</Link>
+          <button>Register</button>
         </div>
       </div>
     )
 }
-export default hhome  
+
+export default register
